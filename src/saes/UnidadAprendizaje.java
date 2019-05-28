e7b53e9a22a214c3216c16433bfbe631a335d528
Rodríguez Cabrera Yoshua <>
2019-05-28 14:53:23 -0500

    Se completaron los métodos de Set y Get y se terminó el constructor de la clase.

--- src/saes/UnidadAprendizaje.java
+++ src/saes/UnidadAprendizaje.java
@@ -3,26 +3,28 @@ package saes;
 public class UnidadAprendizaje {
 
   private String nombre;
-
   private int id;
-
   private GestionEscolar gestion;
   
   public String getNombre() {
-  return null;
+  return nombre;
   }
 
   public void setNombre(String N) {
+      nombre = new String(N);
   }
 
   public int getID() {
-  return 0;
+      return id;
   }
 
   public void setID(int ID) {
+      id = ID;
   }
 
   public UnidadAprendizaje(String N, int ID) {
+      nombre = new String(N);
+      id = ID;
   }
 
 }
