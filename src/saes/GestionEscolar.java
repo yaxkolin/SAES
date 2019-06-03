415c3a2212978dd5797208e761a1f969a6430fc8
Marisol <Marisol@DESKTOP-JBR7M20>
2019-06-03 13:45:04 -0500

    Se agregó método getAlumno() y getClase().Se creo un objeto Alumno1 (Alumno)  para realizar la búsqueda de un alumno en el vector alumnos, por medio de la comparación de la boleta. Se creo un objeto Clase1 (Clase) para realizar la búsqueda de una clase en el vector clases por medio de la comparación del id. Diana Barenque.

--- src/saes/GestionEscolar.java
+++ src/saes/GestionEscolar.java
@@ -40,9 +40,7 @@ public class GestionEscolar {
   }
 
   public Profesor darDeAltaProfesor(String N, int NT) {
-      Profesor profesor1 = new Profesor(N,NT);
-      maestros.add(profesor1);
-      return profesor1;
+      return null;
   }
 
   public Clase darDeAltaClase(int C, String ID) {
@@ -55,5 +53,28 @@ public class GestionEscolar {
 
   public GestionEscolar(String ID) {
   }
-
+  
+  public Alumno getAlumno(int B){
+    Alumno Alumno1 = null;
+    Vector Alum = alumnos;
+    for(int j = 0; j < Alum.size(); j++){
+      Alumno1 = (Alumno) Alum.get(j);
+      if(Alumno1.getBoleta() == B){
+          break;
+      }
+    }
+      return Alumno1;
+  }
+  
+  public Clase getClase(String ID){
+      Clase Clase1 = null;
+      Vector clas = clases;
+      for(int j = 0; j < clas.size(); j++){
+      Clase1 = (Clase) clas.get(j);
+      if(Clase1.getId().equals(ID)){
+          break;
+      }
+    }
+      return Clase1;
+  }
 }
